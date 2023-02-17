26b160d24bbf09f484ccb92c967004865902dc67
= <=>
2023-02-17 13:28:22 +0700

    refactor: create swagger for create lesson

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 4001;
+const LOCALHOST_URL = `http://localhost:${PORT}`;
+const SWAGGER_URL = `${LOCALHOST_URL}/api-docs/`;
 
-export { PORT, MONGO_URL };
+export { PORT, MONGO_URL, LOCALHOST_URL, SWAGGER_URL };
