fd68862c012e2ee6f3492b7dec95c277c9197929
_ahhtuyet_ <anhtuyetdutk19@gmail.com>
2023-02-21 17:02:20 +0700

    feat: Delete Register

--- src/controllers/RegisterController.js
+++ src/controllers/RegisterController.js
@@ -1,6 +1,11 @@
-import { checkExistedCourseId } from "../services/crudDatabase/course.js";
 import {
+  checkExistedCourseId,
+  deleteCourseById
+} from "../services/crudDatabase/course.js";
+import {
+  checkRegisterById,
   checkRegisteredCourse,
+  deleteManyRegisters,
   registerCourse
 } from "../services/crudDatabase/register.js";
 
@@ -38,6 +43,72 @@ const RegisterController = {
       });
     } catch (error) {
       return res.status(400).send({
+        status: "Fail",
+        error: error,
+        data: null
+      });
+    }
+  },
+
+  deleteRegister: async (req, res) => {
+    const registerId = req.param.registerId;
+    const isExistedRegister = await checkRegisterById(registerId);
+
+    if (isExistedRegister === false) {
+      return res.status(400).json({
+        status: "Fail",
+        error: "Register isn't found",
+        data: null
+      });
+    }
+
+    try {
+      console.log(registerId);
+      const output = await deleteCourseById(registerId);
+
+      if (output) {
+        return res.status(200).json({
+          status: "Success",
+          error: null,
+          data: output
+        });
+      } else {
+        return res.status(400).json({
+          status: "Fail",
+          error: null,
+          data: null
+        });
+      }
+    } catch (error) {
+      return res.status(400).json({
+        status: "Fail",
+        error: error,
+        data: null
+      });
+    }
+  },
+
+  deleteRegisters: async (req, res) => {
+    const registerIds = req.body.registerIds;
+    try {
+      const output = await deleteManyRegisters(registerIds);
+      const deleteCount = output.deletedCount;
+
+      if (deleteCount) {
+        return res.status(200).json({
+          status: "Success",
+          error: null,
+          data: deleteCount
+        });
+      } else {
+        return res.status(400).json({
+          status: "Fail",
+          error: null,
+          data: null
+        });
+      }
+    } catch (error) {
+      return res.status(400).json({
         status: "Fail",
         error: error,
         data: null
--- src/routes/RegisterRouter.js
+++ src/routes/RegisterRouter.js
@@ -3,5 +3,7 @@ import RegisterController from "../controllers/RegisterController.js";
 const router = express.Router();
 
 router.post("/", RegisterController.registerNewCourse);
+router.delete("/:registerId", RegisterController.deleteRegister);
+router.delete("/", RegisterController.deleteRegisters);
 
 export default router;
