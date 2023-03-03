0fb3b1d40d342dd0c20d611e68d9caf4837cfc9c
huynhducthanhtuan <tuan40655@gmail.com>
2023-03-03 14:11:59 +0700

    ci: 🎡 update node.js.yaml

--- src/configs/connectDatabase/index.js
+++ src/configs/connectDatabase/index.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../../constants/index.js";
 
-const connectDatabase = () => {
+const connectDatabase = async () => {
   mongoose
     .set("strictQuery", false)
     .connect(MONGO_URI)
     .then(() => {
       console.log("Connect to database success");
+      return true;
     })
     .catch((error) => {
       console.log("Connect to database fail", error);
+      return false;
     });
 };
 
