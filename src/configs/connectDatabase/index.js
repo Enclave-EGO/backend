01c230dffc6157eb1a400830a2214a265a350632
huynhducthanhtuan <tuan40655@gmail.com>
2023-03-03 13:07:23 +0700

    ci: 🎡 update src/index.js

--- src/configs/connectDatabase/index.js
+++ src/configs/connectDatabase/index.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "../../constants/index.js";
 
-const connectDatabase = (server) => {
+const connectDatabase = () => {
   mongoose
     .set("strictQuery", false)
     .connect(MONGO_URI)
     .then(() => {
       console.log("Connect to database success");
-
-      // Stop server to pass CI step
-      server.close();
     })
     .catch((error) => {
       console.log("Connect to database fail", error);
